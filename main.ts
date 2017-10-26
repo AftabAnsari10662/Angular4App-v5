ec7ab8d6ce9c1cee253d2e729d03a5da50b81bf5
Aftab Ansari <aftabansari10662@gmail.com>
2017-10-26 18:46:28 +0530

    Routing

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+import { AppComponent } from './app.component';
 import { NgModule } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { BrowserModule } from "@angular/platform-browser";
@@ -6,13 +7,37 @@ import { FormsModule } from "@angular/forms";
 import { SimpleComponent } from "./simple.component";
 import { MovieList } from "./movie-list.component";
 import { HttpClientModule } from "@angular/common/http";
-
+import { RouterModule } from "@angular/router";
 import { MovieData } from "./movie.data";
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [SimpleComponent, MovieList],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: "simple",
+          component: SimpleComponent
+        },
+        {
+          path: "list",
+          component: MovieList
+        }
+        // ,
+        // {
+        //   path: "**",
+        //   component: MovieList
+        // }
+      ],
+      {
+        useHash: true
+      }
+    )
+  ],
+  declarations: [SimpleComponent, MovieList, AppComponent],
   providers: [MovieData],
-  bootstrap: [SimpleComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
 
